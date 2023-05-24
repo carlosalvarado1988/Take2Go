592fa8ac68456b14951aafc96a3cac8baeca8ca4
Carlos Alvarado <c.alvarado_@hotmail.com>
2023-05-24 15:55:32 -0500

    refactor to scale navigation patterns

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import { Text } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
-import { Ionicons } from "@expo/vector-icons";
 
-import { BusinessScreen } from "./src/features /business/screens/business.screen";
-import { SafeAreaViewContainer } from "./src/components/utilities/safe-area.component";
+import { Navigation } from "./src/infrastructure/navigation";
 import { LocationContextProvider } from "./src/services/location/location.context";
-
 import { RestaurantContextProvider } from "./src/services/restaurants/restaurants.context";
 import {
   useFonts as useOswald,
@@ -18,47 +12,6 @@ import {
 } from "@expo-google-fonts/oswald";
 
 import { theme } from "./src/infrastructure/theme";
-
-const Tab = createBottomTabNavigator();
-
-const TAB_ICON = {
-  Business: "md-restaurant",
-  Map: "md-map",
-  Settings: "md-settings",
-};
-
-function SettingsScreen() {
-  return (
-    <SafeAreaViewContainer
-      // eslint-disable-next-line react-native/no-inline-styles
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <Text>Settings!</Text>
-    </SafeAreaViewContainer>
-  );
-}
-
-function MapScreen() {
-  return (
-    <SafeAreaViewContainer
-      // eslint-disable-next-line react-native/no-inline-styles
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <Text>Map!</Text>
-    </SafeAreaViewContainer>
-  );
-}
-
-const createScreenOptions = ({ route }) => {
-  const iconName = TAB_ICON[route.name];
-  return {
-    tabBarIcon: ({ size, color }) => (
-      <Ionicons name={iconName} size={size} color={color} />
-    ),
-    tabBarActiveTintColor: "tomato",
-    tabBarInactiveTintColor: "gray",
-  };
-};
 
 export default function App() {
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
@@ -73,13 +26,7 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <LocationContextProvider>
           <RestaurantContextProvider>
-            <NavigationContainer>
-              <Tab.Navigator screenOptions={createScreenOptions}>
-                <Tab.Screen name="Business" component={BusinessScreen} />
-                <Tab.Screen name="Map" component={MapScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-              </Tab.Navigator>
-            </NavigationContainer>
+            <Navigation />
           </RestaurantContextProvider>
         </LocationContextProvider>
       </ThemeProvider>
---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import { SafeAreaViewContainer } from "../../components/utilities/safe-area.component";
+import { BusinessScreen } from "../../features /business/screens/business.screen";
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Business: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
+function SettingsScreen() {
+  return (
+    <SafeAreaViewContainer
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <Text>Settings!</Text>
+    </SafeAreaViewContainer>
+  );
+}
+
+function MapScreen() {
+  return (
+    <SafeAreaViewContainer
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <Text>Map!</Text>
+    </SafeAreaViewContainer>
+  );
+}
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+  };
+};
+
+export const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={createScreenOptions}>
+        <Tab.Screen name="Business" component={BusinessScreen} />
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
