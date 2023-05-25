db4c20759de135c1fc48cb8c283c146fd0700a1d
Carlos Alvarado <c.alvarado_@hotmail.com>
2023-05-25 09:45:09 -0500

    adding map region

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,19 +1,38 @@
-import React from "react";
-import MapView from "react-native-maps";
+import React, { useContext, useState, useEffect } from "react";
 
+import { Map } from "./map-screen.styles";
 import { Search } from "../components/search.component";
-import styled from "styled-components/native";
 
-const Map = styled(MapView)`
-  height: 100%;
-  width: 100%;
-`;
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantContext } from "../../../services/restaurants/restaurants.context";
 
 export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantContext);
+  const [latDelta, setLatDelta] = useState(0);
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
   return (
     <>
       <Search />
-      <Map />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.03, // determined zoom
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return null;
+        })}
+      </Map>
     </>
   );
 };
