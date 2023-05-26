363f87ae3438755287200fcdc6cb09b2c458dd0d
Carlos Alvarado <c.alvarado_@hotmail.com>
2023-05-26 11:12:53 -0500

    Setup Firebase account for project and initializeApp config

--- App.js
+++ App.js
@@ -2,17 +2,28 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { initializeApp } from "firebase/app";
 
 import { Navigation } from "./src/infrastructure/navigation";
 import { FavoritesContextProvider } from "./src/services/favorites/favorites.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { RestaurantContextProvider } from "./src/services/restaurants/restaurants.context";
+import { theme } from "./src/infrastructure/theme";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 
-import { theme } from "./src/infrastructure/theme";
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ0UUsWHbSoV5DgjPfVhlRzlHXgu-6-Gc",
+  authDomain: "take2go-93862.firebaseapp.com",
+  projectId: "take2go-93862",
+  storageBucket: "take2go-93862.appspot.com",
+  messagingSenderId: "390361688647",
+  appId: "1:390361688647:web:b990b918909e89d5379f62",
+};
+
+initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
