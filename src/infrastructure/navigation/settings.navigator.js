d6e883e470a4eb5071e41ca710dc36a3aa631720
Carlos Alvarado <c.alvarado_@hotmail.com>
2023-06-02 12:12:09 -0500

    baseline for favorites screen

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -5,19 +5,20 @@ import {
 } from "@react-navigation/stack";
 
 import { SettingsScreen } from "../../features/settings/screens/settings.screen";
+import { FavoritesScreen } from "../../features/settings/screens/favorites.screen";
 
 const SettingsStack = createStackNavigator();
 
 export const SettingsNavigator = () => {
   return (
     <SettingsStack.Navigator
-      headerMode="screen"
       screenOptions={{
         cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        headerMode: "screen",
       }}
     >
       <SettingsStack.Screen name="Settings" component={SettingsScreen} />
-      <SettingsStack.Screen name="Favorites" component={() => null} />
+      <SettingsStack.Screen name="Favorites" component={FavoritesScreen} />
     </SettingsStack.Navigator>
   );
 };
